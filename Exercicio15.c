08917289ecd73a8b2fc0f27c86377a5858604a51
GabFraga <87135402+Gab1203@users.noreply.github.com>
2023-11-09 13:22:47 -0300

    Exercicio15.c

--- Exercicio15.c
+++ Exercicio15.c
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <time.h>
 
-/*Exercício 15:
+/*ExercÃ­cio 15:
 Implemente um programa que converta valores entre diferentes unidades de medida, como por exemplo,
-quilômetros para milhas e Celsius para Fahrenheit. O programa deve permitir ao usuário escolher a conversão desejada e repetir o processo para vários valores.
-Utilize o switch case  e continue mostrando as opções para conversão até o usuário digite 0 para sair do programa.*/
+quilÃ´metros para milhas e Celsius para Fahrenheit. O programa deve permitir ao usuÃ¡rio escolher a conversÃ£o desejada e repetir o processo para vÃ¡rios valores.
+Utilize o switch case  e continue mostrando as opÃ§Ãµes para conversÃ£o atÃ© o usuÃ¡rio digite 0 para sair do programa.*/
 
 int main()
 {
@@ -26,53 +26,53 @@ int main()
 
     do
     {
-        printf("\nTABELA DE CONVERSÕES\n");
-        printf("\n( 1 ) - Quilômetros(Km) - Milhas(Mi)");
-        printf("\n( 2 ) - Milhas(Mi) - Quilômetros(km)");
-        printf("\n( 3 ) - Celsius(°C) - Fahrenheit(°F)");
-        printf("\n( 4 ) - Fahrenheit(°F) - Celsius(°C)");
+        printf("\nTABELA DE CONVERSÃ•ES\n");
+        printf("\n( 1 ) - QuilÃ´metros(Km) - Milhas(Mi)");
+        printf("\n( 2 ) - Milhas(Mi) - QuilÃ´metros(km)");
+        printf("\n( 3 ) - Celsius(Â°C) - Fahrenheit(Â°F)");
+        printf("\n( 4 ) - Fahrenheit(Â°F) - Celsius(Â°C)");
 
-        printf("\nInsira a opção desejada: \n");
+        printf("\nInsira a opÃ§Ã£o desejada: \n");
         scanf("%d", &opcao);
 
         switch (opcao)
         {
         case 1:
 
-            printf("\nInsira a distância em Quilômetros(km): \n");
+            printf("\nInsira a distÃ¢ncia em QuilÃ´metros(km): \n");
             scanf("%f", &quilometros);
             conversaoParaMilhas = quilometros / 1.609;
-            printf("Conversão para Milhas(Mi): %.1f ", conversaoParaMilhas);
+            printf("ConversÃ£o para Milhas(Mi): %.1f ", conversaoParaMilhas);
 
             break;
 
         case 2:
-            printf("\nInsira a distância em Milhas(Mi): \n");
+            printf("\nInsira a distÃ¢ncia em Milhas(Mi): \n");
             scanf("%f", &milhas);
             conversaoParaQuilometros = milhas * 1.609;
-            printf("Conversão para Quilômetros(km): %.1f ", conversaoParaQuilometros);
+            printf("ConversÃ£o para QuilÃ´metros(km): %.1f ", conversaoParaQuilometros);
 
             break;
 
         case 3:
-            printf("\nInsira a temperatura em Celsius(°C): \n");
+            printf("\nInsira a temperatura em Celsius(Â°C): \n");
             scanf("%f", &celsius);
-            conversaoParaCelsius = ((fahrenheit - 32) * 5 / 9);
-            printf("Conversão para Fahrenheit (°F): %.1f ", conversaoParaFahrenheit);
+            conversaoParaFahrenheit = ((celsius * 9 / 5) + 32);
+            printf("ConversÃ£o para Fahrenheit (Â°F): %.1f ", conversaoParaFahrenheit);
 
             break;
 
         case 4:
-            printf("\nInsira a temperatura em Fahrenheit(°F): \n");
+            printf("\nInsira a temperatura em Fahrenheit(Â°F): \n");
             scanf("%f", &fahrenheit);
-            conversaoParaFahrenheit = ((celsius * 9 / 5) + 32);
-            printf("Conversão para Celsius(°C): %.1f ", conversaoParaCelsius);
+            conversaoParaCelsius = ((fahrenheit - 32) * 5 / 9);
+            printf("ConversÃ£o para Celsius(Â°C): %.1f ", conversaoParaCelsius);
             break;
         case 0:
             printf("\nLeitura finalizada!\n");
             break;
         default:
-            printf("Opção inválida, reescreva!");
+            printf("OpÃ§Ã£o invÃ¡lida, reescreva!");
             Sleep(3000);
             system("cls || clear");
             break;
